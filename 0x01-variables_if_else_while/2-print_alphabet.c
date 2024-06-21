aaf510114a89d50fc2b9e386615e07ecba23a857
Elijah_Ibharalu <ibharluelijah@gmail.com>
2024-06-21 13:32:49 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - the main
+ *
+ * Return: alphabet 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
