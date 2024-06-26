21f946855a95c7e70c2111695841950d84625c0d
Elijah_Ibharalu <ibharluelijah@gmail.com>
2024-06-26 11:20:22 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -15,12 +15,12 @@ void times_table(void)
 		{
 			result = i * j;
 			if (j == 0)
-				printf("%d,", result);
+				printf("%d, ", result);
 			else
 			{
 				printf("%2d", result);
 				if (j != 9)
-					printf(",  ");
+					printf(", ");
 			}
 		}
 		printf("\n");
