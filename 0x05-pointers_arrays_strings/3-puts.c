f56d4cb5b95bbaef842ab2c2693f42b00252dd5e
Elijah_Ibharalu <ibharluelijah@gmail.com>
2024-07-01 15:20:37 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _puts - function that prints a dtring
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0 ; str[i] != '\0' ; i++)
+		_putchar(str[i]);
+	_putchar('\n');
+}
